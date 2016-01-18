f621bb45a0cdc3fb5fa44d7de575979a82980497
Carlos Castro Brenes <ccasbre27@hotmail.com>
2016-01-18 13:30:38 -0600

    init

---
+++ DemoSendGrid/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using SendGrid;
+
+namespace DemoSendGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            sendEmail();
+            return View();
+        }
+
+        private void sendEmail()
+        {
+            // Create the email object first, then add the properties.
+            var myMessage = new SendGridMessage();
+
+            // Add the message properties.
+            myMessage.From = new MailAddress("myname@example.com");
+
+            // Add multiple addresses to the To field.
+            List<String> recipients = new List<String>
+            {
+                @"Bacon Ipsum <user@domain.com>",
+                @"Lorem Ipsum <user@domain.com>"
+            };
+
+            myMessage.AddTo(recipients);
+
+            myMessage.Subject = "Testing the SendGrid Library on Sir-Rover!";
+
+            //Add the HTML and Text bodies
+            myMessage.Html = "<p>Hello World from SendGrid!</p>";
+            myMessage.Text = "Hello World!";
+
+
+            // Create credentials, specifying your user name and password.
+var credentials = new NetworkCredential("username", "password");
+
+// Create an Web transport for sending email.
+var transportWeb = new Web(credentials);
+
+// Send the email, which returns an awaitable task.
+transportWeb.DeliverAsync(myMessage);
+        }
+
+    }
+}
---
+++ DemoSendGrid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoSendGrid.Startup))]
+namespace DemoSendGrid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
