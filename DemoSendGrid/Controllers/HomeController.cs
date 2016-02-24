85f0d02ad28a0c8d5175d1801cd37ac95469da9c
Carlos Castro Brenes <ccasbre27@users.noreply.github.com>
2016-02-24 12:31:20 -0600

    Tabulación y nuevos comentarios
    Se cambia los datos de la función sendEmail

--- DemoSendGrid/Controllers/HomeController.cs
+++ DemoSendGrid/Controllers/HomeController.cs
@@ -30,39 +30,42 @@ namespace DemoSendGrid.Controllers
             return View();
         }
 
-        private void sendEmail()
-        {
-            // Create the email object first, then add the properties.
-            var myMessage = new SendGridMessage();
-
-            // Add the message properties.
-            myMessage.From = new MailAddress("myname@example.com");
-
-            // Add multiple addresses to the To field.
-            List<String> recipients = new List<String>
-            {
-                @"Bacon Ipsum <user@domain.com>",
-                @"Lorem Ipsum <user@domain.com>"
-            };
-
-            myMessage.AddTo(recipients);
-
-            myMessage.Subject = "Testing the SendGrid Library on Sir-Rover!";
-
-            //Add the HTML and Text bodies
-            myMessage.Html = "<p>Hello World from SendGrid!</p>";
-            myMessage.Text = "Hello World!";
-
-
-            // Create credentials, specifying your user name and password.
-var credentials = new NetworkCredential("username", "password");
-
-// Create an Web transport for sending email.
-var transportWeb = new Web(credentials);
-
-// Send the email, which returns an awaitable task.
-transportWeb.DeliverAsync(myMessage);
-        }
-
-    }
+        private async void sendEmail()
+		{
+			// Creamos el objeto de tipo Email
+			SendGridMessage myMessage = new SendGridMessage();
+		
+			// Se agrega el remitente del email
+			myMessage.From = new MailAddress("myname@example.com");
+		
+			// Se crea una lista con los destinarios
+			List<String> recipients = new List<String>
+			{
+				@"Bacon Ipsum <user@domain.com>",
+				@"Lorem Ipsum <user@domain.com>"
+			};
+			
+			// se agregan los destinarios
+			myMessage.AddTo(recipients);
+			
+			// se agrega el tema del email
+			myMessage.Subject = "Testing the SendGrid Library on Sir-Rover!";
+		
+			// Se agrega el contenido del mensaje, cabe destacar que se puede hacer mediante texto o html
+			myMessage.Html = "<p>Hello World from SendGrid!</p>";
+			//myMessage.Text = "Hello World!";
+		
+		
+			// Creamos las credenciales de nuestro servicio, enviamos :
+			// 1 - el nombre de usuario que copiamos de azure
+			// 2 - clave que ingresamos cuando se creo el servicio
+			var credentials = new NetworkCredential("username", "password");
+		
+			// Creamos Web transport para enviar el email
+			var transportWeb = new Web(credentials);
+			
+			// enviamos el email, nótese que es una tarea asíncrona por lo que utilizamos el await
+			await transportWeb.DeliverAsync(myMessage);
+		
+		}
 }
